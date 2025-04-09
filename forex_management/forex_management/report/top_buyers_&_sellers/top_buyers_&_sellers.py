e487ac7623c7fbbf0dcf4bc01c9ba4a05f28d27d
natnaelab <natnael08@protonmail.com>
2025-04-09 12:01:44 +0300

    wip

--- forex_management/forex_management/report/top_buyers_&_sellers/top_buyers_&_sellers.py
+++ forex_management/forex_management/report/top_buyers_&_sellers/top_buyers_&_sellers.py
@@ -16,10 +16,9 @@ def execute(filters: dict | None = None):
     """
     columns = get_columns()
     data = get_data(filters=filters)
-    chart = get_chart(filters=filters)
     summary_report = get_summary_report(filters=filters)
 
-    return columns, data, None, chart, summary_report
+    return columns, data, None, None, summary_report
 
 
 def get_columns() -> list[dict]:
@@ -80,21 +79,7 @@ def get_data(filters: dict | None) -> list[list]:
     The report data is a list of rows, with each row being a list of cell values.
     """
 
-    filter_opts = {}
-
-    if filters.get("customer"):
-        filter_opts["customer"] = filters["customer"]
-    if filters.get("transaction_type"):
-        filter_opts["transaction_type"] = filters["transaction_type"]
-    if filters.get("currency"):
-        filter_opts["currency"] = filters["currency"]
-
-    if filters.get("from_date") and filters.get("to_date"):
-        filter_opts["date_and_time"] = ["between", [filters["from_date"], filters["to_date"]]]
-    elif filters.get("from_date"):
-        filter_opts["date_and_time"] = [">=", filters["from_date"]]
-    elif filters.get("to_date"):
-        filter_opts["date_and_time"] = ["<=", filters["to_date"]]
+    filter_opts = get_filters(filters) | {"transaction_type": filters.get("transaction_type")}
 
     transactions = frappe.db.get_all(
         "Transaction",
@@ -128,71 +113,36 @@ def get_data(filters: dict | None) -> list[list]:
     return data
 
 
-def get_chart(filters: dict | None) -> dict:
-    return
-    all_data = get_data(filters=filters)
-    customers = list({row["customer"] for row in all_data})
+def get_filters(filters: dict | None) -> dict:
+    filter_opts = {}
 
-    buy_values = []
-    sell_values = []
+    if filters.get("customer"):
+        filter_opts["customer"] = filters["customer"]
+    if filters.get("currency"):
+        filter_opts["currency"] = filters["currency"]
 
-    for customer in customers:
-        customer_buys = [
-            row["amount_etb"] for row in all_data if row["customer"] == customer and row["transaction_type"] == "Buy"
-        ]
+    if filters.get("from_date") and filters.get("to_date"):
+        filter_opts["date_and_time"] = ["between", [filters["from_date"], filters["to_date"]]]
+    elif filters.get("from_date"):
+        filter_opts["date_and_time"] = [">=", filters["from_date"]]
+    elif filters.get("to_date"):
+        filter_opts["date_and_time"] = ["<=", filters["to_date"]]
 
-        customer_sells = (
-            row["amount_etb"] for row in all_data if row["customer"] == customer and row["transaction_type"] == "Sell"
-        )
-
-        buy_values.append(customer_buys)
-        sell_values.append(customer_sells)
-
-    return {
-        "data": {
-            "labels": customers,
-            "datasets": [
-                {"name": _("Top Buyers"), "values": buy_values},
-                {"name": _("Top Sellers"), "values": sell_values},
-            ],
-        },
-        "type": "bar",
-        "colors": ["#743ee2", "#e2743e"],
-    }
+    return filter_opts
 
 
 def get_summary_report(filters: dict | None) -> dict:
     def _get_top_customer(transaction_type):
-        filter_opts = {}
+        filter_opts = get_filters(filters) | {"transaction_type": transaction_type}
 
-        filter_opts["transaction_type"] = transaction_type
-
-        if filters.get("customer"):
-            filter_opts["customer"] = filters["customer"]
-        if filters.get("currency"):
-            filter_opts["currency"] = filters["currency"]
-
-        if filters.get("from_date") and filters.get("to_date"):
-            filter_opts["date_and_time"] = ["between", [filters["from_date"], filters["to_date"]]]
-        elif filters.get("from_date"):
-            filter_opts["date_and_time"] = [">=", filters["from_date"]]
-        elif filters.get("to_date"):
-            filter_opts["date_and_time"] = ["<=", filters["to_date"]]
-
-        top_customer_transactions = frappe.db.get_all(
+        customer, currency, amount, exchange_rate = frappe.db.get_value(
             "Transaction",
             filters=filter_opts,
-            fields=["customer", "currency", "amount", "exchange_rate"],
-            order_by="amount desc",
-            group_by="customer",
-            limit=1,
+            fieldname=["customer", "currency", "amount", "exchange_rate"],
         ) or [{}]
 
-        top_customer_transactions = top_customer_transactions[0]
-        amount = top_customer_transactions.get("amount", 0)
-        exchange_rate = top_customer_transactions.get("exchange_rate", 0)
-        result = amount * exchange_rate
-        return f"{top_customer_transactions.get('customer','')} </br> ({result:,.2f} ETB)"
+        amount_etb = amount * exchange_rate
+        return f"{customer} </br> ({amount_etb:,.2f} ETB)"
 
     top_buyer = _get_top_customer("Buy")
     top_seller = _get_top_customer("Sell")
